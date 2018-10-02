9ab9b40f8d9ee24db25bbd497d2fe1bf61f32f6f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-02 20:33:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,5 @@
+function createPost() {
+  var pageTemp = _.template(document.getElementById("page-template").innerHTML);
+  var postTemp = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementsById("comments-template").innerHTML);
+}

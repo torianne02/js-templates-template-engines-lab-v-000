a4c2fc01e3cb45d2913677324c722486230f21b6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-02 20:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function createPost() {
+  // create templates
   var pageTemp = _.template(document.getElementById("page-template").innerHTML);
   var postTemp = _.template(document.getElementById("post-template").innerHTML);
-  var commentsTemplate = _.template(document.getElementsById("comments-template").innerHTML);
+  var commentsTemp = _.template(document.getElementById("comments-template").innerHTML);
+
+  // grab blog content
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 }
